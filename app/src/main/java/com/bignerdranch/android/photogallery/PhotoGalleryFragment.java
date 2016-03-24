50c44bfb90af963e42c2269cc69f10d820c8b7c8
p623419 <perkins.joel@heb.com>
2016-03-24 13:34:03 -0500

    -Getting basic setup for fragments.

---
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.java
@@ -0,0 +1,14 @@
+package com.bignerdranch.android.photogallery;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Joel on 3/24/2016.
+ */
+public class PhotoGalleryFragment extends Fragment {
+
+    public static Fragment newInstance(){
+        return new PhotoGalleryFragment();
+    }
+
+}
